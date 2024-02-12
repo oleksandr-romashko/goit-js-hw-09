0c0ab6806827e72ebfa74403b3cdf951bead8cb5
Oleksandr Romashko <alex.rmshk@gmail.com>
2024-02-12 12:53:34 +0100

    Add task-1 solution

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const colorSwither = document.querySelector('.color-switcher');
+let timerId = null;
 
+colorSwither.addEventListener('click', ({ target }) => {
+  if (target.hasAttribute('data-start')) {
+    target.nextElementSibling.toggleAttribute('disabled');
+    target.toggleAttribute('disabled');
+    timerId = setInterval(
+      () => (document.body.style.backgroundColor = getRandomHexColor()),
+      1000
+    );
+  }
+
+  if (target.hasAttribute('data-stop')) {
+    clearInterval(timerId);
+    target.toggleAttribute('disabled');
+    target.previousElementSibling.toggleAttribute('disabled');
+  }
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
